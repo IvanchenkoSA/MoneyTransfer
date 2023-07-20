0c23b3176cd7d70fc17d5b959fe93856f1af56d7
IvanchenkoSA <sergey.1vanchenko@ya.ru>
2023-07-20 11:41:03 +0300

    Исправил

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 fun main() {
-
     val user1 = User("Nikolay", balance = 235000, age = 32, CardType.MASTERCARD)
     val user2 = User("Sergey", balance = 369009, age = 23, CardType.VISA)
     val user3 = User("Ivan", balance = 446980, age = 29)
@@ -36,21 +35,33 @@ data class User(
     var totalSum: Int = 0,
 ) {
     fun getComission(cardType: CardType, totalSum: Int, amountTransfer: Int): Double? {
-        val minSum = 35.0
+        val lim = 600000
+        val lowLim = 150000
+        val minSum = 35
         return when (cardType) {
             CardType.MASTERCARD, CardType.MAESTRO -> if ((amountTransfer + totalSum >= 75000) &&
-                (amountTransfer < 150000 || totalSum + amountTransfer < 600000)
-            ) (amountTransfer / 100) * 0.6 + 20
-            else null
+                (amountTransfer < lowLim || totalSum + amountTransfer < lim)
+            ) {
+                (amountTransfer / 100) * 0.6 + 20
+            } else {
+                null
+            }
 
-            CardType.VISA, CardType.MIR
-            -> if (amountTransfer < 150000 || totalSum + amountTransfer < 600000)
-                if ((amountTransfer / 100) * 0.75 < minSum) minSum
-                else (amountTransfer / 100) * 0.75
-            else null
+            CardType.VISA, CardType.MIR -> if (amountTransfer < lowLim || totalSum + amountTransfer < lim) {
+                if ((amountTransfer / 100) * 0.75 < minSum) {
+                    minSum
+                } else{
+                    (amountTransfer / 100) * 0.75
+                }
+            } else {
+                null
+            }?.toDouble()
 
-            CardType.VKPAY -> if (amountTransfer > 15000 || totalSum + amountTransfer > 40000) null
-            else 0.toDouble()
+            CardType.VKPAY -> if (amountTransfer > 15000 || totalSum + amountTransfer > 40000) {
+                null
+            } else {
+                0.toDouble()
+            }
         }
     }
 }
