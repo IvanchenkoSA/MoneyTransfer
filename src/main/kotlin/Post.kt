a1caf2e4ac60f7012989455eb2f54a7a26c66af4
IvanchenkoSA <sergey.1vanchenko@ya.ru>
2023-08-09 13:52:24 +0300

    Revision

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -4,67 +4,55 @@ fun main() {
         11,
         2342352,
         "Hello World",
-        Likes(168, userLikes = true, canLikes = true, canPublish = true),
-        Views(40),
-        Geo("123124"),
-        Comments(10),
-        "post"
     )
     var post2 = Post(
         1,
         11,
         345612,
         "aboba",
-        Likes(28, userLikes = true, canLikes = true, canPublish = true),
-        Views(90),
-        Geo("234565"),
-        Comments(15),
-        "post"
     )
     var post3 = Post(
         2,
         11,
         793455,
-        "вапва",
-        Likes(45, userLikes = true, canLikes = true, canPublish = true),
-        Views(159),
-        Geo("234565"),
-        Comments(100),
-        "post"
+        "фищиф",
     )
     var ws = WallService()
     ws.add(post1)
     ws.add(post2)
     ws.add(post3)
-    ws.posts.forEach { i -> println(i) }
+    ws.printPosts()
     println(ws.posts.size)
-    println(ws.update(post3))
-    ws.posts.forEach { i -> println(i) }
-
-
+    println(ws.update(Post(1,10,1000023, "New Post")))
+    ws.printPosts()
 }
 
 class WallService {
     var posts = emptyArray<Post>()
+    private var id = 1
 
     fun add(post: Post): Post {
-        posts += post.copy(id = post.id + 1)
+        posts += post.copy(id = id)
+        id++
         return posts.last()
     }
 
-    fun update(post: Post): Boolean {
-        val index = posts.indexOfFirst { p -> p.id == post.id }
+    fun update(newPost: Post): Boolean {
+        val index = posts.indexOfFirst { p -> p.id == newPost.id }
         if (index == -1) return false else {
             posts[index] = posts[index].copy(
-                id = post.id + 100,
-                text = post.text + "plus",
-                likes = post.likes,
-                views = post.views,
-                comments = post.comments,
-                postType = "llllllllllllllll"
+                id = id,
+                ownerId = newPost.ownerId,
+                date = newPost.date,
+                text = newPost.text
             )
+            id++
         }
         return true
+    }
+
+    fun printPosts() {
+        posts.forEach { i -> println(i) }
     }
 
 }
@@ -74,11 +62,11 @@ data class Post(
     val ownerId: Int,
     val date: Long,
     val text: String,
-    var likes: Likes,
-    var views: Views,
-    val geo: Geo,
-    var comments: Comments,
-    val postType: String
+    var likes: Likes = Likes(0, userLikes = true, canLikes = true, canPublish = true),
+    var views: Views = Views(0),
+    val geo: Geo = Geo("01.00000, 01.00000"),
+    var comments: Comments = Comments(0),
+    val postType: String = "Post"
 )
 
 data class Likes(
--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,59 +1,46 @@
 package ru.netology
 
-import Comments
-import Geo
-import Likes
 import Post
-import Views
 import WallService
-
-
 import org.junit.Assert.*
 import org.junit.Test
 
 class WallServiceTest {
-    private val ws = WallService()
     private var post = Post(
         0,
         0,
         1234523,
         "asd",
-        Likes(10, true, true, true),
-        Views(20),
-        Geo("2352345"),
-        Comments(20),
-        "post")
+        )
 
     private var post1 = Post(
         10,
         0,
         1234523,
         "asd",
-        Likes(10, true, true, true),
-        Views(20),
-        Geo("2352345"),
-        Comments(20),
-        "post")
-
+        )
 
     @Test
     fun `get post with id not equals zero(add)`() {
+        val ws = WallService()
         var post2 = ws.add(post)
         assertNotEquals(post2.id, 0)
     }
 
     @Test
     fun `Update post with existing id`(){
+        val ws = WallService()
         val post3 = ws.add(post1)
         val isExisting = ws.update(post3)
-        println(ws.update(post3))
+        println(isExisting)
         assertEquals(true, isExisting)
     }
 
     @Test
     fun `Update post without existing id`(){
+        val ws = WallService()
         val isExisting = ws.update(post1)
-        println(ws.update(post1))
+        println(isExisting)
         assertEquals(false, isExisting)
     }
 }
